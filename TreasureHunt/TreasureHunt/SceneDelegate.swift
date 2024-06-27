b476d92521f60b02e7458851f68ba0f7c097dd27
Max Gabriel <Max.Gabriel@triosstudent.com>
2024-06-26 22:03:47 -0400

    commented the code

--- TreasureHunt/TreasureHunt/SceneDelegate.swift
+++ TreasureHunt/TreasureHunt/SceneDelegate.swift
@@ -1,39 +1,55 @@
 import UIKit
 import CoreLocation
 
+// SceneDelegate class responsible for managing the app's scenes
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
+    // Property to hold the window instance
     var window: UIWindow?
+    
+    // Array to store treasures
     var treasures = [Treasure]()
     
+    // Called when the scene is about to be connected to the app
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        // Load treasures from persistent storage
         loadTreasures()
+        // Ensure the scene is a UIWindowScene instance
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
+    // Called when the scene will resign active state
     func sceneWillResignActive(_ scene: UIScene) {
+        // Save treasures to persistent storage
         saveTreasures()
     }
 
+    // Called when the scene enters the background
     func sceneDidEnterBackground(_ scene: UIScene) {
+        // Save treasures to persistent storage
         saveTreasures()
     }
     
+    // Function to save treasures to persistent storage
     func saveTreasures() {
         let encoder = JSONEncoder()
+        // Encode the treasures array to JSON data
         if let encoded = try? encoder.encode(treasures) {
+            // Store the encoded data in UserDefaults
             UserDefaults.standard.set(encoded, forKey: "treasures")
         }
     }
     
+    // Function to load treasures from persistent storage
     func loadTreasures() {
+        // Retrieve the encoded treasures data from UserDefaults
         if let savedTreasures = UserDefaults.standard.object(forKey: "treasures") as? Data {
             let decoder = JSONDecoder()
+            // Decode the data back to an array of Treasure objects
             if let loadedTreasures = try? decoder.decode([Treasure].self, from: savedTreasures) {
                 treasures = loadedTreasures
             }
-       } 
-            else {
+        } else {
             // Load default treasures if none are saved
             treasures = [
                 Treasure(name: "Case of Money", coordinate: CLLocationCoordinate2D(latitude: 43.68974781, longitude: -79.33157735)),
--- TreasureHunt/TreasureHunt/TreasureList.swift
+++ TreasureHunt/TreasureHunt/TreasureList.swift
@@ -2,10 +2,13 @@ import UIKit
 import MapKit
 import CoreLocation
 
+// Table view controller class responsible for displaying and managing a list of treasures
 class TreasureList: UITableViewController, CLLocationManagerDelegate {
     
+    // CLLocationManager instance to handle location updates
     let locationManager = CLLocationManager()
     
+    // Computed property to get and set treasures from the SceneDelegate
     var treasures: [Treasure] {
         get {
             let sceneDelegate = UIApplication.shared.connectedScenes.first?.delegate as! SceneDelegate
@@ -18,20 +21,27 @@ class TreasureList: UITableViewController, CLLocationManagerDelegate {
         }
     }
     
+    // Property to store the index of the selected treasure
     var selectedTreasureIndex: Int?
 
+    // Called after the controller's view is loaded into memory
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Set the location manager's delegate to self
         locationManager.delegate = self
+        // Request location authorization
         locationManager.requestWhenInUseAuthorization()
         
+        // Register a cell class for the table view
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "TreasureCell")
         
+        // Create an add button and set its action to open the map view
         let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(openMapView))
         navigationItem.rightBarButtonItem = addButton
     }
     
+    // Function to open the map view
     @objc func openMapView() {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let mapVC = storyboard.instantiateViewController(withIdentifier: "MapViewController") as! MapViewController
@@ -40,10 +50,12 @@ class TreasureList: UITableViewController, CLLocationManagerDelegate {
     
     // MARK: - Table view data source
     
+    // Function to return the number of rows in the table view
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return treasures.count
     }
     
+    // Function to configure the cell for a given row
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TreasureCell", for: indexPath)
         let treasure = treasures[indexPath.row]
@@ -53,11 +65,13 @@ class TreasureList: UITableViewController, CLLocationManagerDelegate {
     
     // MARK: - Table view delegate
     
+    // Function called when a row is selected
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         selectedTreasureIndex = indexPath.row
         checkLocationAuthorization()
     }
     
+    // Function to check location authorization status
     func checkLocationAuthorization() {
         switch locationManager.authorizationStatus {
         case .notDetermined:
@@ -71,6 +85,7 @@ class TreasureList: UITableViewController, CLLocationManagerDelegate {
         }
     }
     
+    // CLLocationManagerDelegate function called when location updates are available
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let selectedTreasureIndex = selectedTreasureIndex {
             let treasureLocation = CLLocation(latitude: treasures[selectedTreasureIndex].coordinate.latitude, longitude: treasures[selectedTreasureIndex].coordinate.longitude)
@@ -91,16 +106,19 @@ class TreasureList: UITableViewController, CLLocationManagerDelegate {
         }
     }
     
+    // CLLocationManagerDelegate function called when location updates fail
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print("Failed to get user location: \(error.localizedDescription)")
     }
     
+    // CLLocationManagerDelegate function called when authorization status changes
     func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
         if manager.authorizationStatus == .authorizedWhenInUse || manager.authorizationStatus == .authorizedAlways {
             locationManager.requestLocation()
         }
     }
     
+    // Function to open the map for a specific treasure
     func openMap(for treasureIndex: Int) {
         let treasure = treasures[treasureIndex]
         let coordinate = treasure.coordinate
@@ -116,12 +134,14 @@ class TreasureList: UITableViewController, CLLocationManagerDelegate {
         mapItem.openInMaps(launchOptions: options)
     }
     
+    // Function to show an alert if location access is denied
     func showLocationDeniedAlert() {
         let alert = UIAlertController(title: "Location Access Denied", message: "Location access is required to find the treasures. Please enable location services in settings.", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
         present(alert, animated: true, completion: nil)
     }
 
+    // Function to add a new treasure to the list
     func addNewTreasure(treasure: Treasure) {
         treasures.append(treasure)
         tableView.reloadData()
